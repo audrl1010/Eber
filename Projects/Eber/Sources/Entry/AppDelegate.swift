f1ff99604b6bc3def37260b54799f789d8c99556
손명기 <audrl1010@naver.com>
2019-09-18 17:52:43 +0900

    Generate Xcode project

---
+++ Projects/Eber/Sources/Entry/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  asdfasdfasfasdfasdf
+//
+//  Created by myung gi son on 18/09/2019.
+//  Copyright © 2019 Myung Gi Son. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions
+    launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    return true
+  }
+}
+
