da60a1477bf4298ac3cf2d21d9b472716e5ff2f9
손명기 <audrl1010@naver.com>
2019-09-20 02:27:29 +0900

    Create AuthFixture.swift

---
+++ Projects/Eber/Tests/Models/AuthFixture.swift
@@ -0,0 +1,13 @@
+//
+//  AuthFixture.swift
+//  Eber
+//
+//  Created by myung gi son on 19/09/2019.
+//
+
+import Foundation
+@testable import Eber
+
+enum AuthFixture {
+  static let auth = Auth(id: "eber", password:"eber")
+}
