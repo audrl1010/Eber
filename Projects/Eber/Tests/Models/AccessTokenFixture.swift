b66434dd2794dc33ca93f77d04de7b41514a02d0
손명기 <audrl1010@naver.com>
2019-09-21 23:31:30 +0900

    Create AccessTokenFixture

---
+++ Projects/Eber/Tests/Models/AccessTokenFixture.swift
@@ -0,0 +1,13 @@
+//
+//  AccessTokenFixture.swift
+//  Eber
+//
+//  Created by myung gi son on 21/09/2019.
+//
+
+import Foundation
+@testable import Eber
+
+enum AccessTokenFixture {
+  static let accessToken = AccessToken(accessToken: "accessToken")
+}
