f8d0b7380445543c1026588256cf904d59d938ea
손명기 <audrl1010@naver.com>
2019-09-22 02:14:12 +0900

    Construct Colors and Font in Eber

---
+++ Projects/Eber/Sources/Utils/Font.swift
@@ -0,0 +1,33 @@
+//
+//  Font.swift
+//  Eber
+//
+//  Created by myung gi son on 22/09/2019.
+//
+
+import UIKit
+
+extension CGFloat {
+  var appleSDGothicNeoFont: AppleSDGothicNeoFont {
+    return AppleSDGothicNeoFont(size: self)
+  }
+  var notoSansCJKKR: NotoSansCJKKR {
+    return NotoSansCJKKR(size: self)
+  }
+}
+
+struct NotoSansCJKKR {
+  let size: CGFloat
+  var regular: UIFont { return UIFont(name: "NotoSansCJKkr-Regular", size: self.size)! }
+}
+
+struct AppleSDGothicNeoFont {
+  let size: CGFloat
+  var thin: UIFont { return UIFont(name: "AppleSDGothicNeo-Thin", size: self.size)! }
+  var light: UIFont { return UIFont(name: "AppleSDGothicNeo-Light", size: self.size)! }
+  var regular: UIFont { return UIFont(name: "AppleSDGothicNeo-Regular", size: self.size)! }
+  var bold: UIFont { return UIFont(name: "AppleSDGothicNeo-Bold", size: self.size)! }
+  var semibold: UIFont { return UIFont(name: "AppleSDGothicNeo-SemiBold", size: self.size)! }
+  var ultraLight: UIFont { return UIFont(name: "AppleSDGothicNeo-UltraLight", size: self.size)! }
+  var medium: UIFont { return UIFont(name: "AppleSDGothicNeo-Medium", size: self.size)! }
+}
---
+++ Projects/Eber/Sources/Utils/UIColor+Eber.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Eber.swift
+//  Eber
+//
+//  Created by myung gi son on 22/09/2019.
+//
+
+import UIKit
+import SwiftyColor
+
+extension UIColor {
+  static let black_18: UIColor = 0x000000.color ~ 18%
+  static let black_60: UIColor = 0x000000.color ~ 60%
+  static let black_87: UIColor = 0x000000.color ~ 87%
+  static let white_87: UIColor = 0xffffff.color ~ 87%
+}
