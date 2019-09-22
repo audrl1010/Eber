751a6d84486551528d4ebb4cad49497c0025d85d
손명기 <audrl1010@naver.com>
2019-09-22 20:13:37 +0900

    Add Colors (white_42, shamrock_42)

--- Projects/Eber/Sources/Utils/UIColor+Eber.swift
+++ Projects/Eber/Sources/Utils/UIColor+Eber.swift
@@ -13,5 +13,7 @@ extension UIColor {
   static let black_60: UIColor = 0x000000.color ~ 60%
   static let black_87: UIColor = 0x000000.color ~ 87%
   static let white_87: UIColor = 0xffffff.color ~ 87%
+  static let white_42: UIColor = 0xffffff.color ~ 42%
   static let shamrock: UIColor = 0x00bc45.color
+  static let shamrock_42: UIColor = 0x00bc45.color ~ 42%
 }
