96fa26f5b0687d12d879ba81140ccde04448e714
손명기 <audrl1010@naver.com>
2019-09-24 00:12:54 +0900

    Update Encodable -> Codable
    update code to map id key -> userId key

--- Projects/Eber/Sources/Models/Auth.swift
+++ Projects/Eber/Sources/Models/Auth.swift
@@ -7,8 +7,16 @@
 
 import Foundation
 
-struct Auth: Encodable {
+struct Auth {
   var id: String
   var password: String
   let deviceType: String = "ios"
+}
+
+extension Auth: Codable {
+  enum CodingKeys: String, CodingKey {
+    case id = "userId"
+    case password
+    case deviceType
+  }
 }
