76caf49188596a365577fb1976f1555d0f204657
손명기 <audrl1010@naver.com>
2019-09-26 18:43:32 +0900

    Add UI Layout Extension + Add extension Getting BoundingRect from String

---
+++ Projects/Eber/Sources/Utils/Snap.swift
@@ -0,0 +1,26 @@
+//
+//  CeilUtil.swift
+//  Eber
+//
+//  Created by myung gi son on 21/09/2019.
+//
+
+import UIKit
+
+/// Ceil to snap pixel
+func snap(_ x: CGFloat) -> CGFloat {
+  let scale = UIScreen.main.scale
+  return ceil(x * scale) / scale
+}
+
+func snap(_ point: CGPoint) -> CGPoint {
+  return CGPoint(x: snap(point.x), y: snap(point.y))
+}
+
+func snap(_ size: CGSize) -> CGSize {
+  return CGSize(width: snap(size.width), height: snap(size.height))
+}
+
+func snap(_ rect: CGRect) -> CGRect {
+  return CGRect(origin: snap(rect.origin), size: snap(rect.size))
+}
