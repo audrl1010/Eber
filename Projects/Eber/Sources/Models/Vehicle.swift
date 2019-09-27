e34dfb03b5be3656ab2d99401bb8bf3139e6115a
손명기 <audrl1010@naver.com>
2019-09-28 01:31:37 +0900

    add search feature & extends modelType, modelStream

--- Projects/Eber/Sources/Models/Vehicle.swift
+++ Projects/Eber/Sources/Models/Vehicle.swift
@@ -7,10 +7,38 @@
 
 import Foundation
 
-struct Vehicle: Codable {
+struct Vehicle: ModelType {
+  enum Event {
+    case updateFavorite(vehicleIdx: Int, isFavorite: Bool)
+  }
   var vehicleIdx: Int
   var description: String
   var favorite: Bool
   var licenseNumber: String
   var capacity: Int // L unit
+}
+
+// Array + Vehicle Extension
+
+extension Vehicle {
+  var hashValue: Int {
+    return self.vehicleIdx
+  }
+}
+
+extension Vehicle {
+  func isContain(query: String, partialMatchTargets targets: [KeyPath<Self, String>]) -> Bool {
+    return targets.filter { target in
+      let value = self[keyPath: target].lowercased().removeWhitespace()
+      return value.isMatch(of: query.lowercased().removeWhitespace())
+    }.count > 0
+  }
+}
+
+extension Array where Element == Vehicle {
+  func search(query: String, partialMatchTargets targets: [KeyPath<Element, String>]) -> [Element] {
+    return self.filter { element in
+      element.isContain(query: query, partialMatchTargets: targets)
+    }
+  }
 }
