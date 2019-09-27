bbd3eb2ff0011f4d99922b131c173a4976cd2546
손명기 <audrl1010@naver.com>
2019-09-28 01:33:44 +0900

    Add search feature to VehicleListViewReactor

--- Projects/Eber/Sources/Domains/Vehicle/VehicleListViewReactor.swift
+++ Projects/Eber/Sources/Domains/Vehicle/VehicleListViewReactor.swift
@@ -27,14 +27,15 @@ final class VehicleListViewReactor: Reactor, FactoryModule {
   
   enum Mutation {
     case setVehicles([Vehicle])
+    case setFilteredSectionItems([SectionItem])
     case setLoading(Bool)
     case setQuery(String)
   }
   
   struct State {
-    var isRefreshing: Bool = false
     var isLoading: Bool = false
     var query: String = ""
+    var sectionItems: [SectionItem] = []
     var sections: [Section] = []
   }
   
@@ -57,8 +58,14 @@ final class VehicleListViewReactor: Reactor, FactoryModule {
       ])
       
     case let .updateQuery(query):
+      guard self.currentState.query != query else { return .empty() }
       guard !self.currentState.isLoading else { return .empty() }
-      return .just(.setQuery(query))
+      return Observable.concat([
+        .just(Mutation.setQuery(query)),
+        self.searchVehicles(query: query)
+          // cancel previous '.updateQuery' action.
+          .takeUntil(self.action.filter(Action.isUpdateQueryAction))
+      ])
     }
   }
   
@@ -68,6 +75,22 @@ final class VehicleListViewReactor: Reactor, FactoryModule {
       .debug()
       .map { vehicles in .setVehicles(vehicles) }
       .catchError(self.errorMutation)
+  }
+  
+  private func searchVehicles(query: String) -> Observable<Mutation> {
+    let isFiltering = !query.isEmpty
+    if isFiltering {
+      let vehicles = self.currentState.sectionItems.map { sectionItem in
+        sectionItem.cellReactor.vehicle
+      }
+      let filteredVehicles = vehicles.search(
+        query: query,
+        partialMatchTargets: [\.licenseNumber, \.description]
+      )
+      return .just(.setFilteredSectionItems(self.sectionItems(from: filteredVehicles)))
+    } else {
+      return .just(.setFilteredSectionItems(self.currentState.sectionItems))
+    }
   }
   
   private func errorMutation(error: Error) -> Observable<Mutation> {
@@ -82,20 +105,35 @@ final class VehicleListViewReactor: Reactor, FactoryModule {
     
     switch mutation {
     case let .setVehicles(vehicles):
-      let sectionItems: [SectionItem] = vehicles
-        .map(self.dependency.cellReactorFactory.create)
-        .map(SectionItem.init)
-      let sections = [
-        VehicleListViewSection(items: sectionItems)
-      ]
-      newState.sections = sections
+      let sectionItems = self.sectionItems(from: vehicles)
+      newState.sectionItems = sectionItems
+      newState.sections = [VehicleListViewSection(items: sectionItems)]
       
     case let .setLoading(isLoading):
       newState.isLoading = isLoading
       
     case let .setQuery(query):
       newState.query = query
+      
+    case let .setFilteredSectionItems(filteredSectionItems):
+      newState.sections = [VehicleListViewSection(items: filteredSectionItems)]
     }
     return newState
+  }
+  
+  private func sectionItems(from vehicles: [Vehicle]) -> [SectionItem] {
+    return vehicles
+    .map(self.dependency.cellReactorFactory.create)
+    .map(SectionItem.init)
+  }
+}
+
+extension VehicleListViewReactor.Action {
+  static func isUpdateQueryAction(_ action: VehicleListViewReactor.Action) -> Bool {
+    if case .updateQuery = action {
+      return true
+    } else {
+      return false
+    }
   }
 }
--- Projects/Eber/Tests/Domain/Vehicle/VehicleListViewReactorSpec.swift
+++ Projects/Eber/Tests/Domain/Vehicle/VehicleListViewReactorSpec.swift
@@ -14,26 +14,31 @@ import Stubber
 class VehicleListViewReactorSpec: QuickSpec {
   
   override func spec() {
-    func createReactor(vehicleService: VehicleServiceStub = .init()) -> VehicleListViewReactor {
-      let cellReactorFactory = VehicleCellReactor.Factory(
-        dependency: .init()
+    var vehicleService: VehicleServiceStub!
+    var reactor: VehicleListViewReactor!
+    var cellReactorFactory: VehicleCellReactor.Factory!
+    
+    beforeEach {
+      vehicleService = VehicleServiceStub()
+      let alertService = AlertServiceStub()
+      
+      let favoriteButtonViewReactorFactory = VehicleFavoriteButtonViewReactor.Factory.stub(
+        vehicleService: vehicleService,
+        alertService: alertService
+      )
+      cellReactorFactory = VehicleCellReactor.Factory.stub(
+        vehicleService: vehicleService,
+        alertService: alertService,
+        favoriteButtonViewReactorFactory: favoriteButtonViewReactorFactory
       )
       let factory = VehicleListViewReactor.Factory.init(
         dependency: .init(
           vehicleService: vehicleService,
-          alertService: AlertServiceStub(),
+          alertService: alertService,
           cellReactorFactory: cellReactorFactory
         )
       )
-      return factory.create()
-    }
-    
-    var vehicleService: VehicleServiceStub!
-    var reactor: VehicleListViewReactor!
-    
-    beforeEach {
-      vehicleService = VehicleServiceStub()
-      reactor = createReactor(vehicleService: vehicleService)
+      reactor = factory.create()
     }
     
     describe("an initial state") {
@@ -47,11 +52,71 @@ class VehicleListViewReactorSpec: QuickSpec {
         expect(reactor.currentState.sections).to(beEmpty())
       }
     }
-
+    
     context("when receives a refresh action") {
       it("fetches vehicles") {
+        let vehicles = [VehicleFixture.vehicle1, VehicleFixture.vehicle2]
+        Stubber.register(vehicleService.vehicles) { _ in .just(vehicles) }
         reactor.action.onNext(.refresh)
         expect(Stubber.executions(vehicleService.vehicles).count) == 1
+        expect(reactor.currentState.sectionItems[0].cellReactor.vehicle.vehicleIdx) === vehicles[0].vehicleIdx
+        expect(reactor.currentState.sectionItems[1].cellReactor.vehicle.vehicleIdx) === vehicles[1].vehicleIdx
+      }
+    }
+    
+    context("when has fetched vehicle 2") {
+      beforeEach {
+        let vehicles = [VehicleFixture.vehicle1, VehicleFixture.vehicle2]
+        Stubber.register(vehicleService.vehicles) { _ in .just(vehicles) }
+        reactor.action.onNext(.refresh)
+      }
+      
+      context("when receives a updateQuery action") {
+        it("set query") {
+          let query = "vehicle"
+          reactor.action.onNext(.updateQuery(query))
+          expect(reactor.currentState.query) === query
+        }
+        context("when has query = '333'") {
+          it("has filtered 2 sectionItems") {
+            let query = "333"
+            reactor.action.onNext(.updateQuery(query))
+            expect(reactor.currentState.sections[0].items.count) == 2
+          }
+        }
+        context("when has query = 'vehicle'") {
+          it("has filtered 2 sectionItems") {
+            let query = "vehicle"
+            reactor.action.onNext(.updateQuery(query))
+            expect(reactor.currentState.sections[0].items.count) == 2
+          }
+        }
+        context("when has query = '99'") {
+          it("has filtered 1 sectionItems") {
+            let query = "99"
+            reactor.action.onNext(.updateQuery(query))
+            expect(reactor.currentState.sections[0].items.count) == 1
+          }
+        }
+      }
+    }
+    
+    describe("state.isLoading") {
+      context("while fetching vehicles") {
+        it("is loading") {
+          Stubber.register(vehicleService.vehicles) { _ in .never() }
+          reactor.action.onNext(.refresh)
+          expect(reactor.currentState.isLoading) == true
+        }
+      }
+      
+      context("when finished fetching vehicles") {
+        it("is not loading") {
+          let vehicles = [VehicleFixture.vehicle1, VehicleFixture.vehicle2]
+          Stubber.register(vehicleService.vehicles) { _ in .just(vehicles) }
+          reactor.action.onNext(.refresh)
+          expect(reactor.currentState.isLoading) == false
+        }
       }
     }
   }
