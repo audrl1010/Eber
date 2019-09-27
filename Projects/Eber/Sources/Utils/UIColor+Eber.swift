ac5ad20668ff701b64fd8cd2a0f8c1914c106da6
손명기 <audrl1010@naver.com>
2019-09-28 01:29:33 +0900

    Add black_38 to UIColor

--- Projects/Eber/Sources/Utils/UIColor+Eber.swift
+++ Projects/Eber/Sources/Utils/UIColor+Eber.swift
@@ -10,6 +10,7 @@ import SwiftyColor
 
 extension UIColor {
   static let black_18: UIColor = 0x000000.color ~ 18%
+  static let black_38: UIColor = 0x000000.color ~ 38%
   static let black_60: UIColor = 0x000000.color ~ 60%
   static let black_87: UIColor = 0x000000.color ~ 87%
   static let white_87: UIColor = 0xffffff.color ~ 87%
